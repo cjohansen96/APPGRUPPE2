788a936a3491de109b4ada11c95df5eb1c5aa720
Markus W. Byremo <byremo93@hotmail.com>
2019-01-29 12:00:26 +0100

    Oppdatering

---
+++ registration.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0">
+	<link rel="stylesheet" type="text/css" href="css/style_registration.css">
+	<title>Exizt Registration</title>
+</head>
+<body>
+
+
+	<div class="container">
+		<div class="Brand">
+			<img src="#" alt="">
+		</div>
+		<div class="grid">
+			<section class="section-1">
+				<h1>Sign Up</h1>
+				<p>Already have an account?</p>
+				<div class="logIn-link">
+					<a href="">Log In!</a>
+				</div>
+				<h4>Fill out form</h4>
+			</section>
+
+			<section class="section-2">
+				<form action="">
+					<p>
+						<label>First name</label>
+						<input type="text" name="First name">
+					</p>
+					<p>
+						<label>Last name</label>
+						<input type="text" name="Last name">
+					</p>
+					<p>
+						<label>Email</label>
+						<input type="text" name="E-mail">
+					</p>
+					<p>
+						<label>Adress</label>
+						<input type="text" name="Adress">
+					</p>
+					<p>
+						<label>Password</label>
+						<input type="text" name="Password">
+					</p>
+					<p>
+						<label>Confirm Password</label>
+						<input type="text" name="Confirm-passwrod">
+					</p>
+					<p>
+						<label>Phone number</label>
+						<input type="text" name="Phone">
+					</p>
+					<p>
+						<button>Create an Account</button>
+					</p>
+				</form>
+			</section>
+		
+
+		</div>
+	</div>
+
+</body>
+</html>

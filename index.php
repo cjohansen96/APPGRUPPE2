d0a2d38222d96cfdc1947eede7bcc0d556158dc6
Kristian Olsen <krismo94@hotmail.com>
2019-02-01 16:11:50 +0100

    bytta til grid på index og top.php
    byttet til grid på index og top.php og laget egen css for top.php. Kun skalert for mobil nå.

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
-
+	
 <?php
   require 'top.php';
 ?>
 
-	 <!-- Top --> 
-<header class="masthead">
+<link rel="stylesheet" href="css/style_index.css">
 <body>
 
 <div class="content">
-	<div class="salebox">
-		
+	<div class="card">
+		<p>1</p>
 	</div>
-	<div class="leftbox">
-		
+	<div class="card1">
+		<p>2</p>
 	</div>
-	<div class="rightbox">
-		
+	<div class="card2">
+		<p>3</p>
 	</div>
-	<div class="leftbox">
-		
+	<div class="card3">
+		<p>4</p>
 	</div>
-	<div class="rightbox">
-		
+	<div class="card4">
+		<p>5</p>
 	</div>
 
 </div>
--- top.php
+++ top.php
@@ -5,15 +5,21 @@
 <title>Exizt</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/style_index.css">
+<link rel="stylesheet" href="css/style_top.css">
 </head>
 
 <body>
-	<div class="nav">
+	<div class="grid">
+		<div class="item1">
 			<img src="Bilder/Navmeny.png">
-			<h1 id="Menutxt">Menu</h1>
+		</div>
+		<div class="item2">
+			<h1>Menu</h1>
+		</div>
 	</div>
 </body>
-		
+
+</html>
+
 	
 

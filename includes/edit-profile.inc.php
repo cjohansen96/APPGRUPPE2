bf9b2985a41539d7a7c4c88bfe28c516cf06e357
Unknown <christofferjohansen12@gmail.com>
2019-02-15 15:32:27 +0100

    Begynt på profile edit funksjoner

---
+++ includes/edit-profile.inc.php
@@ -0,0 +1,49 @@
+<?php
+if(isset($_POST['edit-submit'])) {
+
+	require 'dbh.inc.php';
+
+  	$name = $_POST['name'];
+  	$lastName = $_POST['lname'];
+  	$email = $_POST['mail'];
+  	$tlfNumber = $_POST['tlf'];
+
+// Validation
+  	if (empty($name) || empty($lastName) || empty($email) || empty($tlf)) {
+  		header("Location: ../profile.php?error=emptyfields&name".$name."&mail=".$email)
+  	}
+  	else if (!preg_match("/^[a-zA-Z]*$/", $name) && !preg_match("/^[a-zA-Z]*$/", $lastName) && !preg_match("/^[0-9]*$/", $tlfNumber) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    	header("Location: ../registration.php?error=invalidnamelnametlfemail");
+    	exit();
+ 	}
+  	else if (!preg_match("/^[a-zA-Z]*$/", $name)) {
+    	header("Location: ../registration.php?error=invalidname&name=".$name);
+    	exit();
+  	}
+  	else if (!preg_match("/^[a-zA-Z]*$/", $lastName)) {
+    	header("Location: ../registration.php?error=invalidlname&lname=".$LastName);
+    	exit();
+  	}
+  	else if (!preg_match("/^[0-9]*$/", $tlfNumber)) {
+    	header("Location: ../registration.php?error=invalidltlf&tlf=".$tlfNumber);
+    	exit();
+  	}  
+  	else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    	header("Location: ../registration.php?error=invalidmail&mail=".$email);
+    	exit();
+  	}
+
+
+
+
+
+
+
+
+
+
+}
+else {
+	header("Location: ../profile.php");
+	exit();
+}

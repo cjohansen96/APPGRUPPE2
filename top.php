10be9bafc33a951ac87b9257509bf3066e9a503f
Kristian <krismo94@hotmail.com>
2019-03-06 12:18:09 +0100

    Bedre navbar
    mangler logo og farger giggity

--- top.php
+++ top.php
@@ -1,31 +1,57 @@
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Exizt</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/style_top.css">
+  <title>Exizt</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/style_top.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
-
 <body>
-	<div class="grid">
-		<div class="item1">
-			<img src="Bilder/Navmeny.png">
-		</div>
-		<div class="item2">
-			<!-- logo -->
-		</div>
-		<div class="item3">
-			<h1>MENS</h1>
-		</div>
-		<div class="item4">
-			<h1>WOMEN</h1>
-		</div>
-	</div>
+
+<nav class="navbar navbar-expand-md navbar-dark navbarbgclr">
+  <div class="container-fluid">
+  <a href="#" class="navbar-brand">Exizt</a>
+
+  <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive">
+    <span class="navbar-toggler-icon ml-auto"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <h2>MEN</h2>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <h2>WOMEN</h2>
+      </a>
+    </li>
+  </ul> 
+  </div>
+      <div id="searchbar">
+      <form class="form-inline" action="/action_page.php">
+    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <button class="btn btn-default" type="submit"> <span class="fas fa-search"></span> </button>
+  </form>
+  </div>
+
+  <button type="button" class="btn btn-default"> <span class="fas fa-user"></span> LOGIN</button>
+
+  <button type="button" class="btn btn-default"> <span class="fas fa-shopping-cart"></span> </button>
+
+</div>
+</nav>
+
+
+
+
+
 </body>
-
 </html>
-
-	
-

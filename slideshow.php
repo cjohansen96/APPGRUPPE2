98a3137bc50213e5fcfa6bd7bb88b292de8ea202
Thomas Hodnebrog <thomas.hodniss@gmail.com>
2019-03-20 15:34:34 +0100

    Slider m/bilder JS og CSS

---
+++ slideshow.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Slideshow</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style_slideshow.css">
+    <script src="js/slideshow.js"></script>
+</head>
+<body>
+   <div class="row">
+    <!-- Slideshow container -->
+<div class="slideshow-container">
+
+        <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade">
+            <script>plusSlides(-1)</script>
+          <div class="numbertext"></div>
+          <img src="bilder/salebanner1.png" style="width:100%">
+          <div class="text"></div>
+        </div>
+      
+        <div class="mySlides fade">
+          <div class="numbertext"></div>
+          <img src="bilder/salebanner2.png" style="width:100%">
+          <div class="text"></div>
+        </div>
+      
+        <div class="mySlides fade">
+          <div class="numbertext"></div>
+          <img src="bilder/salebanner3.png" style="width:100%">
+          <div class="text"></div>
+        </div>
+</div>
+</div>
+</body>
+</html>

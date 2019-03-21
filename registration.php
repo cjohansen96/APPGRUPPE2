9e51d3be23f528d6ebf8229f3976fe1e478124a9
Unknown <christofferjohansen12@gmail.com>
2019-03-21 12:57:32 +0100

    Kan ikke registrere bruker når man er logget inn

--- registration.php
+++ registration.php
@@ -1,3 +1,12 @@
+<?php
+	session_start();
+	if (isset($_SESSION['customerId'])) {
+        header("Location: ../APPGRUPPE2/index.php");
+        exit();
+	}
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">

f171155023b83d75a26cc16b236d80e984fdfd76
Kristian <krismo94@hotmail.com>
2019-03-27 11:55:12 +0100

    Register siden er nå bootstrap

--- registration.php
+++ registration.php
@@ -1,9 +1,9 @@
 <?php
-	session_start();
-	if (isset($_SESSION['customerId'])) {
-        header("Location: ../APPGRUPPE2/index.php");
-        exit();
-	}
+session_start();
+if (isset($_SESSION['customerId'])) {
+	header("Location: ../APPGRUPPE2/index.php");
+	exit();
+}
 
 ?>
 
@@ -13,69 +13,63 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale = 1.0">
-	<link rel="stylesheet" type="text/css" href="css/style_registration.css">
 	<title>Exizt Registration</title>
 </head>
-<body>
+<?php
+require 'top.php';
+?>
+<link rel="stylesheet" type="text/css" href="css/style_reg.css">
+<body style="background: url('Bilder/bg3.jpg') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-size: cover;
+  -o-background-size: cover;">
+	<div class="container-fluid">
+		<div class="row">
 
-
-	<div class="container">
-		
-		<div class="box">
-
-			
-			<section class="section-1">
-
-				<div class="brand">
-					<img src="#" alt="">
-				</div>
+			<div class="col-sm-12 col-md-12 col-lg-12 text-center" style="padding:0px;margin:0px;">
 				<h1>Sign Up</h1>
-				<p>Already have an account? <a href="login.php">Login Here ></a></p>	
-			</section>
-			
-			<section class="section-2">
-				<h4>Fill Out Form</h4>
-				<form action="includes/registration.inc.php" method="post">
-					<p>
-						<input type="text" name="name" placeholder="First name">
-					</p>
-					<p>
-						<input type="text" name="lname" placeholder="Last name">
-					</p>
-					<p>
-						<input type="text" name="mail" placeholder="Email">
-					</p>
-					<p>
-						<input type="text" name="tlf" placeholder="Phone number">
-					</p>
-					<p>
-						<input type="password" name="pwd" placeholder="Password">
-					</p>
-					<p>
-						<input type="password" name="pwd-repeat" placeholder="Confirm Password">
-					</p>
+				<p>Already have an account? 
+					<a href="login.php" id="linktxt">Login Here ></a></p>	
+					<h4>Fill Out Form</h4>
+					<form action="includes/registration.inc.php" method="post" >
+						<p>
+							<input type="text" name="name" placeholder="First name">
+						</p>
+						<p>
+							<input type="text" name="lname" placeholder="Last name">
+						</p>
+						<p>
+							<input type="text" name="mail" placeholder="Email" class="validate">
+						</p>
+						<p>
+							<input type="text" name="tlf" placeholder="Phone number">
+						</p>
+						<p>
+							<input type="password" name="pwd" placeholder="Password">
+						</p>
+						<p>
+							<input type="password" name="pwd-repeat" placeholder="Confirm Password">
+						</p>
 
-					<p>
-					<input type="checkbox" name="checkbox" value="check" id="agree">
-					</p>
-					
-					<p class="check"> I have read and agree to the Terms and<br> Conditions and Privacy Policy</p>
-					
-					
-					<p class="btn">
-						<button type="submit" name="reg-submit">Submit</button>
+						<p>
+							<input type="checkbox" name="checkbox" value="check" id="agree">
+						</p>
 
-					</p>
-				</form>
-			</section>
-			<section class="footer">
+						<p> I have read and agree to the Terms and<br> Conditions and Privacy Policy</p>
 
-				<!-- Whitespace -->
-			</section>
-		
 
+						<p class="btn">
+							<button type="submit" name="reg-submit">Submit</button>
+
+						</p>
+					</form>
+				</div>
+
+			</div>
 		</div>
-	</div>
-
-</body>
-</html>
+	</body>
+	<?php
+	require 'bottom.php';
+	?>
+	</html>

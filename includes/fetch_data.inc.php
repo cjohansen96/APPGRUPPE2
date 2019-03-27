91ee69c45fbf01039aadb9445cf7ab6b51b03dda
Unknown <christofferjohansen12@gmail.com>
2019-03-27 15:31:04 +0100

    Filter test!

---
+++ includes/fetch_data.inc.php
@@ -0,0 +1,76 @@
+<?php
+
+//fetch_data.php
+
+include('dbh.inc.php');
+
+if(isset($_POST["action"]))
+{
+ $query = "
+ SELECT * FROM Clothes WHERE Gender = 'M'
+ ";
+
+if(isset($_POST["brand"]))
+{
+  $brand_filter = implode("','", $_POST["brand"]);
+  $query .= "
+  AND Brand IN('".$brand_filter."')
+  ";
+}
+
+elseif(isset($_POST["category"]))
+{
+  $category_filter = implode("','", $_POST["category"]);
+  $query .= "
+  AND Category IN('".$category_filter."')
+  ";
+}
+
+elseif(isset($_POST["color"]))
+{
+  $color_filter = implode("','", $_POST["color"]);
+  $query .= "
+  AND Color IN('".$color_filter."')
+  ";
+}
+
+
+$result = mysqli_query($conn,$query); 
+$output = '';
+while($row = mysqli_fetch_assoc($result))
+{
+ $output .= '
+
+
+
+      <div style="margin-bottom: 20px;" class="col-md-3 col-sm-6">
+        <form method="post" action="clothing.php?action=add&id='.$row['IdClothes'].'">
+        <div style="height: 450px;" class="card shadow text-center">
+          <div class="card-block">
+            <img src="Bilder/clothes/'. $row['ProductImage'].'" alt="" class="img-fluid" style="height: 250px;">
+            <div class="card-title">
+              <h4>'.$row['Name'].'</h4>
+            </div>
+            <div class="card-text">
+              '.$row['Brand'].'
+            </div>
+            <div class="card-text">
+              '.$row['Quantity'].' in stock
+            </div>
+            <div class="card-text">
+              <h3>£ '.$row['Price'].'</h3>
+            </div>
+            <input style="width: 80%; margin-left: 10%; " type="text" name="quantity" class="form-control" value="1"/>
+            <a style="margin-top: 10px; margin-bottom: 10px;" href="#" class="btn btn-success">Add to cart <span class="fas fa-cart-arrow-down"></span></a>
+          </div>
+        </div>
+      </form>
+      </div>
+ ';
+}
+
+
+echo $output;
+}
+
+?>
--- mensjeans.php
+++ mensjeans.php
@@ -1,98 +1,130 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Mens clothes</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/style_mensclothes.css">
+<head>    
+	<title>Clothes</title>
 </head>
 
 
 <?php
-require 'top.php';
 require 'includes/dbh.inc.php';
+require 'top.php';
 ?>
 
-<body>
-	<div class="container" style="margin-top: 20px; margin-bottom: 20px;">
+<body>   <!-- Page Content -->
+	<div class="container">
 		<div class="row">
+			<div class="col-md-3">     
+				<div class="list-group">
+					<h3>Brand</h3>
+					<div style="height: 180px; overflow-y: auto; overflow-x: hidden;">
+						<?php
 
-			<!-- Filter options -->
+						$query = "SELECT DISTINCT(Brand) FROM Clothes";
+						$result = mysqli_query($conn,$query);
 
-			<!--
-			<div class="col-md-3 col-sm-12">
-				<h1><span class="fas fa-sliders-h" style="font-size: 0.9em;"></span>Filter</h1>
-				<select class="form-control form-control-lg" style="margin-bottom: 10px;">
-					<option>Category</option>
-					<option>T-shirt</option>
-					<option>Sweater</option>
-					<option>Jeans</option>
-				</select>
-				<select class="form-control form-control-lg" style="margin-bottom: 10px;">
-					<option>Brand</option>
-					<option>Exizt</option>
-					<option>Vans</option>
-					<option>The hundreds</option>
-				</select>
-				<select class="form-control form-control-lg" style="margin-bottom: 10px;">
-					<option>Price</option>
-					<option>Low-high</option>
-					<option>High-low</option>	
-				</select>
-				<select class="form-control form-control-lg" style="margin-bottom: 10px;">
-					<option>Color</option>
-					<option>Black</option>
-					<option>Grey</option>
-					<option>White</option>
-				</select>
-				<select class="form-control form-control-lg" style="margin-bottom: 10px;">
-					<option>Size</option>
-					<option>S</option>
-					<option>M</option>
-					<option>L</option>
-				</select>
-			</div>
--->
+						while ($row = mysqli_fetch_assoc($result)) 
+						{
+							?>
+							<div class="list-group-item checkbox">
+								<label><input type="checkbox" class="common_selector brand" value="<?php echo $row['Brand']; ?>"  > <?php echo $row['Brand']; ?></label>
+							</div>
+							<?php
+						}
 
-			<?php
-			$query = 'SELECT * FROM Clothes WHERE Category="Jeans"';
-			$result = mysqli_query($conn,$query);	
-
-			
-			if (mysqli_num_rows($result)>0):
-				while ($Clothes = mysqli_fetch_assoc($result)):
-				?>		
-			<!-- Clothing cards -->
-			<div style="margin-bottom: 20px;" class="col-md-3 col-sm-6">
-				<form method="post" action="clothing.php?action=add&id=<?php echo $Clothes['IdClothes'];?>">
-				<div style="height: 450px;" class="card shadow text-center">
-					<div class="card-block">
-						<img src="Bilder/clothes/<?php echo $Clothes ['ProductImage'];?>" alt="" class="img-fluid" style="height: 250px;">
-						<div class="card-title">
-							<h4><?php echo $Clothes['Name'];?></h4>
-						</div>
-						<div class="card-text">
-							<?php echo $Clothes['Quantity'];?> in stock
-						</div>
-						<div class="card-text">
-							<h3>£ <?php echo $Clothes['Price'];?></h3>
-						</div>
-						<input style="width: 80%; margin-left: 10%; " type="text" name="quantity" class="form-control" value="1"/>
-						<a style="margin-top: 10px; margin-bottom: 10px;" href="#" class="btn btn-success">Add to cart <span class="fas fa-cart-arrow-down"></span></a>
+						?>
 					</div>
 				</div>
-			</form>
+
+				<div class="list-group">
+					<h3>Category</h3>
+					<?php
+
+					$query = "
+					SELECT DISTINCT(Category) FROM Clothes";
+					$result = mysqli_query($conn,$query);
+
+					while ($row = mysqli_fetch_assoc($result)) 
+					{
+						?>
+						<div class="list-group-item checkbox">
+							<label><input type="checkbox" class="common_selector category" value="<?php echo $row['Category']; ?>" > <?php echo $row['Category']; ?></label>
+						</div>
+						<?php    
+					}
+
+					?>
+				</div>
+
+				<div class="list-group">
+					<h3>Color</h3>
+					<?php
+					$query = "
+					SELECT DISTINCT(Color) FROM Clothes";
+					$result = mysqli_query($conn,$query);
+
+					while ($row = mysqli_fetch_assoc($result)) 
+					{
+						?>
+						<div class="list-group-item checkbox">
+							<label><input type="checkbox" class="common_selector color" value="<?php echo $row['Color']; ?>"  > <?php echo $row['Color']; ?></label>
+						</div>
+						<?php
+					}
+					?> 
+				</div>
 			</div>
 
-			<?php
-		endwhile;
-endif;
-			?>
+			<div class="col-md-9">
+				<br />
+				<div class="row filter_data">
+
+				</div>
+			</div>
 		</div>
+
 	</div>
+
+	<script>
+		$(document).ready(function(){
+
+			filter_data();
+
+			function filter_data()
+			{
+				var action = 'fetch_data';
+				var brand = get_filter('brand');
+				var category = get_filter('category');
+				var color = get_filter('color');
+				$.ajax({
+					url:"includes/fetch_data.inc.php",
+					method:"POST",
+					data:{action:action, brand:brand, category:category, color:color},
+					success:function(data){
+						$('.filter_data').html(data);
+					}
+				});
+			}
+
+			function get_filter(class_name)
+			{
+				var filter = [];
+				$('.'+class_name+':checked').each(function(){
+					filter.push($(this).val());
+				});
+				return filter;
+			}
+
+			$('.common_selector').click(function(){
+				filter_data();
+			});
+
+		});
+	</script>
+	<?php
+	require 'bottom.php';
+	?>
+
 </body>
 
-<?php
-require 'bottom.php';
-?>
 
 </html>

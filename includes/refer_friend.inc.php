dffd081dd0e7158f30d0185f82a86208c79903b7
Unknown <christofferjohansen12@gmail.com>
2019-04-11 17:45:44 +0200

    Lagt inn referafriend handler i profile

--- includes/refer_friend.inc.php
+++ includes/refer_friend.inc.php
@@ -3,8 +3,9 @@ session_start();
 require 'dbh.inc.php';
 if(isset($_POST['refer_submit'])){
 	$id = $_SESSION['email'];
-	if($id == $_POST['email']){
-		header("Location: ../APPGRUPPE2/profile.php?error=youremail");
+	$customerid = $_SESSION['customerId'];
+	if($id === $_POST['email']){
+		header("Location: ../profile.php?error=youremail");
 		exit();
 	}
 	else{
@@ -12,15 +13,7 @@ if(isset($_POST['refer_submit'])){
 		$email = isset($_POST['email']) ? $_POST['email'] : ''; 
 
 		 
-	 	//hente ut emailene i customer tabellen og legge de i et array
-		$SQLStatement = "SELECT Email FROM Customer";
-		$result = mysqli_query($conn,$SQLStatement);
-		$emailArray = array();
-		$index = 0;
-		while($row = mysqli_fetch_assoc($result)){
-			$emailArray[$index] = $row;
-			$index++;
-		}
+	 	
 		// henter ut emailene fra refer_friend tabellen.
 		$SQLreferStatement = "SELECT Email FROM Refere_Friend";
 		$referResult = mysqli_query($conn,$SQLreferStatement);
@@ -30,51 +23,41 @@ if(isset($_POST['refer_submit'])){
 			$referArray[$j] = $row;
 			$j++;
 		}
-
-
-		$customerExists = false;
-		//sjekker om emailen ligger i Customer databasen.
-		foreach ($emailArray as $key => $value){
-			foreach($value as $relationship => $mail){
+		
+		//Sjekker om emailen allerede ligger i refer_tabellen. 
+		$referExists = false;
+		foreach ($referArray as $key => $value) {
+			foreach ($value as $relationship => $mail) {
 				if($mail == $email){
-					$customerExists = true;
+						$referExists = true;
 				}
 			}
 		}
-			//Sjekker om emailen allerede ligger i refer_tabellen. 
-			$referExists = false;
-			foreach ($referArray as $key => $value) {
-				foreach ($value as $relationship => $mail) {
-					if($mail == $email){
-						$referExists = true;
-					}
-				}
-			}
 
 			//legger den nye mailen i customer tabellen, og den gamle blir lagt over i refer tabellen i db
-			if($customerExists === true && $referExists === false){
-				$nyrefer = "INSERT INTO Refere_Friend(IdCustomer,Email)
-				SELECT IdCustomer, Email
-				FROM Customer
-				WHERE Email = '$email'";
-					//sjekker om brukeren ble lagt til
-				if($conn->query($nyrefer) == TRUE){
-					 header("Location: ../APPGRUPPE2/profile.php?refere=succsess");
+			if($referExists === false){
+				$sql = "INSERT INTO Refere_Friend(IdCustomer, Email) VALUES (?,?);";
+				$stmt = mysqli_stmt_init($conn);
+				if(!mysqli_stmt_prepare($stmt, $sql)){
+					header("Location: ../profile.php?refereerror=sqlerror");
+					exit();
 				}
-					
-				//error melding hvis brukeren ikke ble lagt til.
 				else {
-						header("Location: ../APPGRUPPE2/profile.php?error=usernotadded");
-				}
-
+					mysqli_stmt_bind_param($stmt, "is", $customerid,$email);
+					mysqli_stmt_execute($stmt);
+					 header("Location: ../profile.php?referesuccsess=emailadded");
+					exit();
+				
+				}	
+				
 			}
 				//mailen finens fra før i refer_friend tabell
 			elseif($customerExists === true && $referExists === true){
-				header("Location: ../APPGRUPPE2/profile.php?error=refertwice");;
+				header("Location: ../profile.php?error=refertwice");;
 			}
 				//mailen finnes ikke i customer tabellen
 			else{
-				header("Location: ../APPGRUPPE2/profile.php?error=emaildonotexist");
+				header("Location: ../profile.php?error=emaildonotexist");
 			}
 	}
 

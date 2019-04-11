7a97c0558b7d17c294a752d0a9a3873763a05246
afaetten <46598504+afaetten@users.noreply.github.com>
2019-04-11 11:24:19 +0200

    Update cart.php
    Små endringer

--- cart.php
+++ cart.php
@@ -3,6 +3,11 @@ session_start();
 require_once('includes/dbh.inc.php'); 
 include('top.php');
 
+if(empty($_SESSION['cart'])){
+	$_SESSION['cart'] = array();
+}
+
+//add to cart
 if(isset($_GET['id']) && !empty($_GET['id'])){
   if(isset($_SESSION['id']) && !empty($_SESSION['id'])){
     $items = $_SESSION['id'];
@@ -27,13 +32,27 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
   }
 
 
+
+$items = $_SESSION['cart'];
+$cartitems = explode(",", $items);
+if(isset($_GET['remove']) & !empty($_GET['remove'])){
+    $delitem = $_GET['remove'];
+    unset($cartitems[$delitem]);
+    $itemids = implode(",", $cartitems);
+    $_SESSION['cart'] = $itemids;
+}
+
 ?>
- 
+
+
+
 <div class="container">
 <?php 
 $items = $_SESSION['cart'];
 $cartitems = explode(",", $items);
-//print_r($cartitems);
+var_dump($_SESSION['cart']);
+
+
 ?>
 	<div class="row">
 	  <table class="table">
@@ -41,28 +60,33 @@ $cartitems = explode(",", $items);
 	  		<th>S.NO</th>
 	  		<th>Item Name</th>
 	  		<th>Price</th>
+	  		<th>Quantity</th>
 	  	</tr>
 		<?php
 		$total = 0;
 		$i=1;
+		if(isset($_SESSION['cart'])){
 		 foreach ($cartitems as $key=>$id) {
 			$sql = "SELECT * FROM Clothes WHERE IdClothes = $id";
 			$res=mysqli_query($conn, $sql);
 			$r = mysqli_fetch_assoc($res);
-		?>	  	
-	  	<tr>
-	  		<td><?php echo $i; ?></td>
-	  		<td>$<?php echo $r['Name']; ?></td>
-	  		<td><a href="delcart.php?remove=<?php echo $key; ?>">Remove</a> <?php echo $r['Price']; ?></td>
-	  	</tr>
-		<?php 
-			$total = $total + $r['Price'];
-			$i++; 
+		  	
+	  		echo '<tr>';
+	  			echo '<td>'. $i .'</td>';
+	  			echo '<td>' .$r['Name']. '</td>';
+	  			echo '<td>€' .$r['Price']. '</td>';
+	  			echo '<td>' .$r['Quantity']. '</td>';
+	  			echo '<td><a href="?remove=' . $key . '">remove</a></td>';
+	  		echo '</tr>';
+		
+				$total = $total + ($r['Quantity']*$r['Price']);
+				$i++; 
 			} 
+		}
 		?>
 		<tr>
 			<td><strong>Total Price</strong></td>
-			<td><strong>$<?php echo $total; ?></strong></td>
+			<td><strong>€<?php echo $total; ?></strong></td>
 			<td><a href="#" class="btn btn-info">Checkout</a></td>
 		</tr>
 	  </table>

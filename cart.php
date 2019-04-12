c45a0029a635f36790601a59a568efec6bad0a3c
chris2g <christofferjohansen12@gmail.com>
2019-04-12 03:02:20 +0200

    Hardkodet litt cart stuff for pres og lagt til cupon

--- cart.php
+++ cart.php
@@ -13,26 +13,26 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
     $items = $_SESSION['id'];
     $cartitems = explode(",", $items);
     if(in_array($_GET['id'], $cartitems)){
-      echo('status=incart');
+     /* echo('status=incart');*/
     }else{
       $items .= "," . $_GET['id'];
       $_SESSION['cart'] = $items;
-      echo('status=success');
+     /* echo('status=success');*/
         
     }
  
   }else{
     $items = $_GET['id'];
     $_SESSION['cart'] = $items;
-    echo('status=success');
+   /* echo('status=success');*/
   }
     
 }else{
-    echo('status=failed');
+   /* echo('status=failed');*/
   }
 
 
-
+/*
 $items = $_SESSION['cart'];
 $cartitems = explode(",", $items);
 if(isset($_GET['remove']) & !empty($_GET['remove'])){
@@ -40,6 +40,41 @@ if(isset($_GET['remove']) & !empty($_GET['remove'])){
     unset($cartitems[$delitem]);
     $itemids = implode(",", $cartitems);
     $_SESSION['cart'] = $itemids;
+    
+}
+*/
+
+/* Spørring for å sjekke om refer a friend er gjort */
+
+$idCustomer = $_SESSION['customerId'];
+$query = "SELECT C.IdCustomer AS Cid, C.First_Name, C.Email, O.IdOrder, O.IdCustomer, RF.refereId AS Rid, RF.Email, RF.IdCustomer AS Rfid FROM Customer AS C INNER JOIN Orders AS O ON C.IdCustomer = O.IdCustomer INNER JOIN Refere_Friend AS RF ON C.Email = RF.Email";
+
+$result = mysqli_query($conn,$query);
+
+$friendRefered = false;
+while ($row = mysqli_fetch_assoc($result)) 
+{
+	if($row['Rfid'] == $idCustomer) {
+		$friendRefered = true;
+	}
+}
+
+if (isset($_POST['checkout'])) {
+        $sql = "INSERT INTO Orders (IdCustomer, IdClothes, Sum_Pris) VALUES (?, ?, ?);";
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+          exit();
+        }
+        else {
+          $clothe = 11;
+          $price = 10;
+          mysqli_stmt_bind_param($stmt, "iii", $idCustomer, $clothe, $price);
+
+          mysqli_stmt_execute($stmt);
+          echo "Order placed!";
+          exit();
+        }
 }
 
 ?>
@@ -47,39 +82,52 @@ if(isset($_GET['remove']) & !empty($_GET['remove'])){
 
 
 <div class="container">
-<?php 
+<?php /*
 $items = $_SESSION['cart'];
 $cartitems = explode(",", $items);
 var_dump($_SESSION['cart']);
+*/
+$test = array("0" => "11");
 
 
 ?>
 	<div class="row">
 	  <table class="table">
 	  	<tr>
-	  		<th>S.NO</th>
+	  		<th>Number</th>
 	  		<th>Item Name</th>
 	  		<th>Price</th>
 	  		<th>Quantity</th>
 	  	</tr>
 		<?php
+		$referCupon = 0.00;
+		if (isset($_POST['refer'])) {
+			$referCupon = 0.30;
+		}
 		$total = 0;
 		$i=1;
 		if(isset($_SESSION['cart'])){
-		 foreach ($cartitems as $key=>$id) {
+		 foreach ($test as $key=>$id) {
 			$sql = "SELECT * FROM Clothes WHERE IdClothes = $id";
 			$res=mysqli_query($conn, $sql);
 			$r = mysqli_fetch_assoc($res);
 		  	
+			$quanityTest = $r['Quantity'] - $r['Quantity'] + 1;
+
 	  		echo '<tr>';
 	  			echo '<td>'. $i .'</td>';
 	  			echo '<td>' .$r['Name']. '</td>';
 	  			echo '<td>€' .$r['Price']. '</td>';
-	  			echo '<td>' .$r['Quantity']. '</td>';
+	  			echo '<td>' .$quanityTest. '</td>';
 	  			echo '<td><a href="?remove=' . $key . '">remove</a></td>';
 	  		echo '</tr>';
-		
-				$total = $total + ($r['Quantity']*$r['Price']);
+				if ($referCupon == 0.00) {
+					$total = $total+(1*$r['Price']);
+				}			
+				else {
+				$discount = $referCupon*($total+(1*$r['Price']));
+				$total = $total+(1*$r['Price'])-$discount;
+				}
 				$i++; 
 			} 
 		}
@@ -87,7 +135,11 @@ var_dump($_SESSION['cart']);
 		<tr>
 			<td><strong>Total Price</strong></td>
 			<td><strong>€<?php echo $total; ?></strong></td>
-			<td><a href="#" class="btn btn-info">Checkout</a></td>
+
+			<form action="cart.php?checkout" method="POST">
+			<td><button type="submit" name="checkout" class="btn btn-info">Checkout</button></td>
+			<td><button type="submit" id="refer-button" name="refer"></button></td>
+			</form>
 		</tr>
 	  </table>
 	  
@@ -96,3 +148,11 @@ var_dump($_SESSION['cart']);
 </div>
  
 <?php include('bottom.php'); ?>
+
+<script>
+	var friendRefered = '<?php echo $friendRefered ?>';
+	if (friendRefered) {
+		$('#refer-button').addClass('btn btn-warning');
+		$('#refer-button').text('30% cupon');
+	}
+</script>
